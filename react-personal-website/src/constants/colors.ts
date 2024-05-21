4b413b399e4c7ee98336c47e5c10dd38fd56073d
Carlos Acosta <carlosra0345@gmail.com>
2024-05-20 21:59:25 -0500

    initial commit

---
+++ react-personal-website/src/constants/colors.ts
@@ -0,0 +1,12 @@
+// colors.ts
+
+export const colors = {
+    darkBlue: "#1A2630",
+    auraBlue: "#223240",
+    plum: "#4b0d49",
+    lightPlum: "#e63ce0",
+    hotMag: "#ff17e4",
+    magenta: "#e310cb",
+    aqua: "#86fbfb", 
+    white: "#f7f8fa",
+}
